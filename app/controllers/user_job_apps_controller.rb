cb91ba7c57476030d24f18d0272998be5b38eaea
yeah-right <claar.zack@gmail.com>
2019-04-17 11:54:24 -0600

    Show page and edit page are being routes to and rendering in the browser properly. Will build them out after I eat some lunch.

--- app/controllers/user_job_apps_controller.rb
+++ app/controllers/user_job_apps_controller.rb
@@ -2,6 +2,7 @@ require 'pry'
 class UserJobAppsController < ApplicationController
 
   def index
+    @user_job_apps = JobApp.all
     @user = current_user
   end
 
@@ -11,18 +12,33 @@ class UserJobAppsController < ApplicationController
   end 
 
   def create
+    # current_user.user_job_app = []
     @user_job_app = UserJobApp.new(user_job_app_params)
     current_user.user_job_app = @user_job_app
     binding.pry
     redirect_to user_job_apps_path
   end 
 
+  def edit 
+    @user_job_app = UserJobApp.find(params[:id])
+  end 
 
+  def update 
+    @user_job_app = UserJobApp.find(params[:id])
+    @user_job_app = UserJobApp.update(user_job_app_params)
+    redirect_to user_job_app_path
+  end
+  
+  def show 
+    @user = current_user 
+    @user_job_app = JobApp.find(params[:id])
+  end 
 
   private 
 
   def user_job_app_params
-    params.require(:user_job_app).permit(:job_apps_attributes => [:job_title, :position, :status, :activity])
+    params.require(:user_job_app).permit(:job_apps_attributes => 
+      [:job_title, :position, :status, :activity])
   end 
 
 end
