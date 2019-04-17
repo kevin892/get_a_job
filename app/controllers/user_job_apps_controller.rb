a2a297eb7cced48eb9ed8fae42744c8e5ef5c4e3
yeah-right <claar.zack@gmail.com>
2019-04-17 11:33:32 -0600

    creating a new job app for the tracker with the nested form is working and being saved to the datebase successfully

--- app/controllers/user_job_apps_controller.rb
+++ app/controllers/user_job_apps_controller.rb
@@ -1,9 +1,28 @@
+require 'pry'
 class UserJobAppsController < ApplicationController
+
   def index
     @user = current_user
   end
 
   def new 
+    @user = current_user
     @user_job_app = UserJobApp.new
   end 
+
+  def create
+    @user_job_app = UserJobApp.new(user_job_app_params)
+    current_user.user_job_app = @user_job_app
+    binding.pry
+    redirect_to user_job_apps_path
+  end 
+
+
+
+  private 
+
+  def user_job_app_params
+    params.require(:user_job_app).permit(:job_apps_attributes => [:job_title, :position, :status, :activity])
+  end 
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,12 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_one :resume
+  has_one :user_job_app
 
   has_many :skills, through: :resume
   has_many :job_histories, through: :resume
   has_many :educations, through: :resume
+  
   accepts_nested_attributes_for :resume
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
